3731c610188a8c84d7ab8bae880482e12e745be4
denervalspelizario <denervalspelizario@gmail.com>
2023-01-30 18:57:59 -0300

    estudo typescript uso de boolean

---
+++ modulo_2/2.2_typeBoolean/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// para transpilar precisa dar ctrl + shift + b  e dar watch e depois ir no powershell e dar node arquivo.js
+// Boolean Type
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// exemplo  2
+let maiorIdade = false;
+if (maiorIdade) {
+    console.log('O usuario é maior de idade');
+}
+else {
+    console.log('Usuário menor de idade');
+}
---
+++ modulo_2/2.2_typeBoolean/app.ts
@@ -0,0 +1,19 @@
+
+// para transpilar precisa dar ctrl + shift + b  e dar watch e depois ir no powershell e dar node arquivo.js
+// Boolean Type
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// exemplo  2
+
+let maiorIdade : boolean = false
+
+if(maiorIdade){
+    console.log('O usuario é maior de idade')
+} else {
+    console.log('Usuário menor de idade')
+}
