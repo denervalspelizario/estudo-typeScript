953330021f7129cc6c5b81d0e0a694addb92b58d
denervalspelizario <denervalspelizario@gmail.com>
2023-02-14 05:54:17 -0300

    estudo de typescript type tupla

---
+++ modulo_2/2.6_Tupla/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// para transpilar precisa dar ctrl + shift + b  e dar watch e depois ir no powershell e dar node arquivo.js
+// a tupla é uma matriz(array) com um numero elementos FIXOS ouse ja tem que ser na ordem certa senão da erro
+// Exemplo 01 - uso simples de Tuplas em Typescript
+let pessoa;
+pessoa = ['Denerval', 'Desenvolvedor Mobile', 36];
+console.log(pessoa);
---
+++ modulo_2/2.6_Tupla/index.ts
@@ -0,0 +1,12 @@
+// para transpilar precisa dar ctrl + shift + b  e dar watch e depois ir no powershell e dar node arquivo.js
+
+
+// a tupla é uma matriz(array) com um numero elementos FIXOS ouse ja tem que ser na ordem certa senão da erro
+
+// Exemplo 01 - uso simples de Tuplas em Typescript
+
+let pessoa : [string, string, number ];
+
+pessoa = ['Denerval', 'Desenvolvedor Mobile', 36];
+
+console.log(pessoa)
