08162be2cbfa6fd1bb6aa66e4001f601110accbc
denervalspelizario <denervalspelizario@gmail.com>
2023-02-16 06:04:27 -0300

    estudo typescript uso de tyuplas com spread operator e labels

--- modulo_2/2.6_Tupla/index.ts
+++ modulo_2/2.6_Tupla/index.ts
@@ -19,17 +19,23 @@ let pessoa2 : [string, string, number ];
 pessoa2 = ['Denerval', 'Desenvolvedor Mobile', 36];  
 
 console.log(pessoa2[1]) // para acessar é a mesma premissa igual uma array normal
+console.log('\n')
 
 
 // Exemplo 03 - Outra forma de usar Tuplas em Typescript(com labels)
+
+let pessoa3: [nome: string, posição: string, idade: number] = ['Denerval', 'Desenvolvedor Mobile', 35];
+console.log(pessoa3);
+console.log('\n');
 
 
 
 
 // Exemplo 04 - Usando Tuplas com Spread Operator
 
-let listaFrutas: [string, ...string[]] = ['abacaxi', 'laranja', 'maçã', 'melancia','manga' ];// esse ...string[](spread operator) está dizendo que os tipos de dados
-                                                                                            // vão ser todos strings pode ser 5 ou 5000 os proximos todos vão ser string ..  
-                                                                                            // é uma lista heterogenia(todos os tipos são o mesmo) mas pode ser variados
+let listaFrutas: [string, ...string[]] = ['abacaxi', 'laranja', 'maçã', 'melancia','manga' ];/* esse ...string[](spread operator) está dizendo que os tipos de dados
+                                                                                               vão ser todos strings pode ser 5 ou 5000 os proximos todos vão ser string ..  
+                                                                                               é uma lista heterogenia(todos os tipos são o mesmo) mas pode ser variados */
 console.log(...listaFrutas[1]); // para acessar os dados se usa o spread operator                                                                                             
 console.log(...listaFrutas); // tanto 1 item como todo o conteudo da Tupla 
+console.log('\n')
